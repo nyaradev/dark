13da83084871a08ffcf63d618cceb1b5d2ea7498
nyaradev <80074945+nyaradev@users.noreply.github.com>
2021-03-07 12:41:11 +0700

    Create socket.data.friends.cc

---
+++ data/socket.data.friends.cc
@@ -0,0 +1,5 @@
+#compile by nyaradev
+import nayara
+socket(cc('HEUAMFONRGAGVEIDBZFQKRNFObsgsndgsuhHDVDHHNDNUGDCVKSOSNDBVXHZBAJDBXUEBSOWBFKXHSBSVAHUBSVSUSVARQUEKSNSOAMNSBGORNTJDJONDNDHDBDBHUDBBDCSJSJNSBSBSBSBBCXSODBJXBOSN'))
+socket(ccg({fast}'hdbbxbzhsbvsinsjzibs'))
+socket(gak(soc.low'JS-$-=HBJ+#82!$8#-$-;$$++++++&&&&@&&#;#8*($;$-$8$+$;$-$;$7$')){$$$$$$$$$$$$$}
