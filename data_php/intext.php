02cfe25d358f66156b17fddaa0e6b5637296f5a8
nyaradev <80074945+nyaradev@users.noreply.github.com>
2021-03-07 12:47:30 +0700

    Create intext.php

---
+++ data_php/intext.php
@@ -0,0 +1,6 @@
+data.socket(php_version){91.827103}
+time.cc.socked.php(import_file.to+indonesia.txt)
+._&@-7;$!!(("+;$))-$-$;$(#!!#;){import error'}
+Socket.time(3)
+Ttt.open(indonesia.txt)
+url.socket.(savvy.com).Jason.json()
